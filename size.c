a45cc8f058bab9a98cf5ec9ef0a70a62d5b77ab2
ncb6206 <62326659+ncb6206@users.noreply.github.com>
2020-04-07 23:58:30 +0900

    Update size.c

--- size.c
+++ size.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-    int **x; //이중포인터 변수 x 선언
+    int **x; //이중포인터 변수 x 선언(int형)
 
     printf("[----- [NA KYU TAE] [2017038073] -----]\n"); //이름과 학번 출력
     printf("sizeof(x) = %lu\n",sizeof(x)); //x의 사이즈 값 출력
