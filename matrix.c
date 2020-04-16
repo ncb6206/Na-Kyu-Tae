cac6b0077ee5005e7004ca87164133b23a6ba056
ncb6206 <62326659+ncb6206@users.noreply.github.com>
2020-04-17 00:27:26 +0900

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -92,7 +92,7 @@ int** create_matrix(int row, int col)
 //create_matrix 함수에 할당된 메모리를 해제하는 함수 
 int free_matrix(int** matrix, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
@@ -102,7 +102,7 @@ int free_matrix(int** matrix, int row, int col)
 		free(matrix[i]); //각 행에 있는 열(col)마다 할당된 메모리 해제
 	free(matrix); //각 행(row)마다 할당된 메모리 해제
 
-	//matrix가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix == NULL) {
 		printf("Matrix not released.\n");
 		return -1;
@@ -136,7 +136,7 @@ void print_matrix(int** matrix, int row, int col)
 //0~19사이의 랜덤값으로 배열을 채우는 함수
 int fill_data(int** matrix, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
@@ -147,7 +147,7 @@ int fill_data(int** matrix, int row, int col)
 		for (int j = 0; j < col; j++)
 			matrix[i][j] = rand() % 20;
 
-	//matrix가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix == NULL) {
 		printf("Matrix is not properly filled\n");
 		return -1;
@@ -159,7 +159,7 @@ int fill_data(int** matrix, int row, int col)
 //matrix의 전치를 matrix_t에 대입하는 함수
 int transpose_matrix(int** matrix, int** matrix_t, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
@@ -169,7 +169,7 @@ int transpose_matrix(int** matrix, int** matrix_t, int row, int col)
 		for (int j = 0; j < col; j++)
 			matrix_t[j][i] = matrix[i][j];
 
-	//matrix_t가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix_t가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix_t == NULL) {
 		printf("Matrix not properly transposed.\n");
 		return -1;
@@ -181,7 +181,7 @@ int transpose_matrix(int** matrix, int** matrix_t, int row, int col)
 //matrix_a와 matrix_b를 더한값을 matrix_sum에 대입하는 함수
 int addition_matrix(int** matrix_a, int** matrix_b, int** matrix_sum, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
@@ -191,7 +191,7 @@ int addition_matrix(int** matrix_a, int** matrix_b, int** matrix_sum, int row, i
 		for (int j = 0; j < col; j++)
 			matrix_sum[i][j] = matrix_a[i][j] + matrix_b[i][j];
 
-	//matrix_sum가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix_sum가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix_sum == NULL) {
 		printf("Matrix did not addition properly.\n");
 		return -1;
@@ -203,7 +203,7 @@ int addition_matrix(int** matrix_a, int** matrix_b, int** matrix_sum, int row, i
 //matrix_a에 matrix_b를 빼준값을 matrix_sub에 대입하는 함수
 int subtraction_matrix(int** matrix_a, int** matrix_b, int** matrix_sub, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
@@ -213,7 +213,7 @@ int subtraction_matrix(int** matrix_a, int** matrix_b, int** matrix_sub, int row
 		for (int j = 0; j < col; j++)
 			matrix_sub[i][j] = matrix_a[i][j] - matrix_b[i][j];
 
-	//matrix_sub가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix_sub가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix_sub == NULL) {
 		printf("Matrix did not subtraction properly.\n");
 		return -1;
@@ -225,15 +225,14 @@ int subtraction_matrix(int** matrix_a, int** matrix_b, int** matrix_sub, int row
 //matrix_a에 matrix_t를 곱해준값을 matrix_axt에 대입하는 함수
 int multiply_matrix(int** matrix_a, int** matrix_t, int** matrix_axt, int row, int col)
 {
-	//행 또는 열의 값이 0이하일 때 NULL을 반환하는 전처리함수
+	//행 또는 열의 값이 0이하일 때 -1을 반환하는 전처리함수
 	if (row <= 0 || col <= 0) {
 		printf("Check the sizes of row and col!\n");
 		return -1;
 	}
 
 	for (int i = 0; i < row; i++) //matrix_a와 matrix_t를 곱하면 rowXrow의 정사각 행렬이 나온다.
-		for (int a = 0; a < row; a++) 
-		{
+		for (int a = 0; a < row; a++) {
 			int sum = 0; //matrix_a의 행과 matrix_t의 열을 곱한값을 더하기 위한 sum변수 선언
 			for (int t = 0; t < col; t++)
 				//matrix_a의 행과 matrix_t의 열을 곱한값을 sum에 더한다.
@@ -241,7 +240,7 @@ int multiply_matrix(int** matrix_a, int** matrix_t, int** matrix_axt, int row, i
 		matrix_axt[i][a] = sum; //sum값을 matrix_axt의 행과 열에 대입
 	}
 
-	//matrix_axt가 NULL일경우 NULL을 반환하는 후처리 함수
+	//matrix_axt가 NULL일경우 -1을 반환하는 후처리 함수
 	if (matrix_axt == NULL) {
 		printf("Matrix did not multiply properly.\n");
 		return -1;
